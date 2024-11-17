fead87775bd67616353046fb6ad2a379f44ee721
cryptoneth <149672650+cryptoneth@users.noreply.github.com>
2024-11-17 20:09:12 +0330

    Update browser.sh

--- browser.sh
+++ browser.sh
@@ -31,8 +31,8 @@ install_chromium() {
             -e CUSTOM_USER=$USERNAME \
             -e PASSWORD=$PASSWORD \
             -e CHROME_CLI=https://www.youtube.com/@IR_TECH/ `#optional` \
-            -p 3010:3000 \
-            -p 3011:3001 \
+            -p 3030:3000 \
+            -p 3031:3001 \
             -v /root/chromium/config:/config \
             --shm-size="1gb" \
             --restart unless-stopped \
@@ -74,8 +74,8 @@ install_firefox() {
             -e TZ=Etc/UTC \
             -e CUSTOM_USER=$USERNAME \
             -e PASSWORD=$PASSWORD \
-            -p 4010:3000 \
-            -p 4011:3001 \
+            -p 4040:3000 \
+            -p 4041:3001 \
             -v /root/firefox/config:/config \
             --shm-size="1gb" \
             --restart unless-stopped \
